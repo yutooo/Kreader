144df2d3c342e14719bc77d688af71c9cb0662f8
yutooo <yutotkns@yahoo.co.jp>
2022-02-18 10:43:58 +0900

    addcomment

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,7 @@
 function flexTextarea(el) {
     const dummy = el.querySelector('.FlexTextarea__dummy')
     el.querySelector('.FlexTextarea__textarea').addEventListener('input', e => {
-      dummy.textContent = e.target.value + '\u200b'
+      dummy.textContent = e.target.value + '\u200b'     //最後の改行が無視されないように
     })
 }
 document.querySelectorAll('.FlexTextarea').forEach(flexTextarea)
